21473f0b7c52fc9fa40374abc52accb16f25045b
John Gietzen <otac0n@gietzen.us>
2013-11-10 00:28:30 -0800

    Added a function for looking up the Path of LDraw.

--- LegoGif/Program.cs
+++ LegoGif/Program.cs
@@ -9,11 +9,35 @@
 namespace LegoGif
 {
     using System;
+    using System.IO;
+    using System.Linq;
+    using Microsoft.Win32;
 
     internal class Program
     {
+        private static readonly Lazy<string> LDPath = new Lazy<string>(() =>
+        {
+            var paths = new string[3];
+
+            using (var key = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\LDView.exe"))
+            {
+                paths[0] = key.GetValue("") as string;
+            }
+
+            if (paths[0] != null)
+            {
+                paths[0] = Path.GetDirectoryName(paths[0]);
+            }
+
+            paths[1] = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles), "LDView");
+            paths[2] = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86), "LDView");
+
+            return paths.FirstOrDefault(p => p != null && Directory.Exists(p));
+        });
+
         public static void Main(string[] args)
         {
+            Console.WriteLine(LDPath.Value);
         }
     }
 }
