ca1c60044623f24dbe83559c543aca6f5fb2ac42
John Gietzen <otac0n@gietzen.us>
2013-11-10 00:09:18 -0800

    Created the LegoGif project.

---
+++ LegoGif/Program.cs
@@ -0,0 +1,19 @@
+// -----------------------------------------------------------------------
+// <copyright file="Program.cs" company="(none)">
+//   Copyright © 2013 John Gietzen.  All Rights Reserved.
+//   This source is subject to the MIT license.
+//   Please see license.md for more information.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace LegoGif
+{
+    using System;
+
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
