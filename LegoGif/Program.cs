d4b1aa96d8ddb400e39e1e0db9a273ff6d4447ac
John Gietzen <otac0n@gietzen.us>
2013-11-10 09:01:29 -0800

    Shell out to POV-Ray to render the frames.

--- LegoGif/Program.cs
+++ LegoGif/Program.cs
@@ -23,6 +23,11 @@ namespace LegoGif
             return GetLDrawPaths().FirstOrDefault(p => p != null && Directory.Exists(p));
         });
 
+        private static readonly Lazy<string> PovRayPath = new Lazy<string>(() =>
+        {
+            return GetPovRayPaths().FirstOrDefault(p => p != null && File.Exists(p));
+        });
+
         public static void Main(string[] args)
         {
             var file = args[0];
@@ -32,11 +37,12 @@ namespace LegoGif
             foreach (var angle in CameraAngles())
             {
                 var frameName = "frame_" + angle;
+                var povFile = frameName + ".pov";
 
                 var l3pStartInfo = new ProcessStartInfo
                 {
                     FileName = "l3p.exe",
-                    Arguments = string.Format("-stdout \"{0}\" {1}.pov -o -cg30,{2}", file, frameName, angle, LDPath.Value),
+                    Arguments = string.Format("-stdout \"{0}\" {1} -o -cg30,{2}", file, povFile, angle, LDPath.Value),
                     CreateNoWindow = true,
                     RedirectStandardOutput = true,
                     UseShellExecute = false,
@@ -45,9 +51,28 @@ namespace LegoGif
 
                 using (var p = Process.Start(l3pStartInfo))
                 {
+                    MakePump(p.StandardOutput, ">")();
+                    p.WaitForExit();
+                }
+
+                var povStartInfo = new ProcessStartInfo
+                {
+                    FileName = PovRayPath.Value,
+                    Arguments = string.Format(" /NR /RENDER \"{0}\" /EXIT +F +FN +UA +W512 +H384 +Q11 +A", povFile),
+                    RedirectStandardError = true,
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                    WindowStyle = ProcessWindowStyle.Minimized,
+                };
+
+                using (var p = Process.Start(povStartInfo))
+                {
+                    Task.Factory.StartNew(MakePump(p.StandardError, "!"));
                     Task.Factory.StartNew(MakePump(p.StandardOutput, ">"));
                     p.WaitForExit();
                 }
+
+                File.Delete(povFile);
             }
         }
 
@@ -74,6 +99,40 @@ namespace LegoGif
 
             yield return Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles), "LDView");
             yield return Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86), "LDView");
+        }
+
+        public static IEnumerable<string> GetPovRayPaths()
+        {
+            var paths = new[]
+            {
+                Path.Combine("POV-Ray", "v3.7", "bin"),
+                Path.Combine("POV-Ray", "v3.6", "bin"),
+                Path.Combine("POV-Ray for Windows v3.6", "v3.6", "bin"),
+            };
+
+            var files = new[]
+            {
+                "pvengine64.exe",
+                "pvengine32-sse2.exe",
+                "pvengine.exe",
+            };
+
+            var roots = new[]
+            {
+                Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles),
+                Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86),
+            };
+
+            foreach (var path in paths)
+            {
+                foreach (var file in files)
+                {
+                    foreach (var root in roots)
+                    {
+                        yield return Path.Combine(root, path, file);
+                    }
+                }
+            }
         }
 
         public static Action MakePump(TextReader reader, string prefix)
